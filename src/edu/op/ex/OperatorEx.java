89745f51576b9a926c4676eff5f1d08fd3e0df07
JOOYEONGJUN <galn7071@naver.com>
2025-08-25 16:29:54 +0900

    operator/삼항연산사/stack/heap

--- src/edu/op/ex/OperatorEx.java
+++ src/edu/op/ex/OperatorEx.java
@@ -37,4 +37,96 @@ public class OperatorEx {
 
 
     }
+   // void 라는 반환할 값이 존재하지 않고, 단순 출력 만 진행하는 기능을 뜻함
+    /*
+    * 자바라는 언어는
+    * (변수)바구니에 들어갈 데이터가
+    * - 숫자, 문자, 사람이 만든 크기, 실수등
+    *   데이터를 담을 변수 이름의 종류 부터
+    *
+    *
+    *
+    * 기능을 실행할떄
+    * - 기능을 실행만 하면 되는 되는것인지?
+    *       public void 기능명칭(매개변수 자리, 파라미터 자리){
+    *           수행할 기능 작성
+    *        }
+    * - 기능을 실행 후 실행 결과를 어딘가에 전달하는 기능인지?
+    *       public 전달할자료형 기능명칭(매개변수 자리, 파라미터 자리){
+    *           수행할 기능 작성
+    *           return 전달할데이터;
+    *        }
+    *
+    * 프로그램을 수행하기 전에 정의 해야할 사항이 많음
+    * */
+
+
+
+    /*
+    * method2() 기능
+    * 첫번재 입력 받은 수가 두번쨰 입력 받은 수의 배수 가 맞는지 확인
+    *
+    *
+    * 변수 이름 : int = input1 , input2
+    *            boolean =  isTrue %  == 0;
+    *   pirntf 활용해서 %d는 %d 의 배수가 맞는가? %b 형태로 출력
+    *
+    * */
+    public  void method2(){
+
+        // Scanner  처럼 동일한 클래스는 최초 1회만 호출 후 여러번 사용  가능
+        // Scanner sc1 =new Scanner(System.in);
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("첫번쨰 숫자를 입력하세요:");
+        int input1 = sc.nextInt();
+
+        System.out.println("두번쨰 숫자를 입력하세요:");
+        int input2 = sc.nextInt();
+
+        boolean isTrue = (input1 % input2 == 0);
+        System.out.printf("%d는 %d의 배수인가 ? : %b" ,input1,input2,isTrue);
+
+    }
+
+    /*
+    * 입력 받은 정수가 짝수인지 홀수인지 판별
+    * 정수입력 : {입력값}
+    * {입력값}은 짝수/홀수 입니다.
+    * */
+    public void  method3(){
+        // void  = return 없음 !! 오직 실행!!!
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("정수 입력 :");
+        int input   = sc.nextInt();
+
+        // 삼항연산자 : 조건식 ? 참: 거짓;
+        boolean isTrue = (input % 2 ==0);
+
+    /*String result = isTrue ? "짝수":"홀수";
+    *
+    * 1. isTrue?"짝수":"홀수"
+    *   boolean isTrue = (input % 2 ==0) 위에서 input 결과가 true / false  로
+    *           isTrue 라는 변수 이름에 담겨짐
+    *
+    * 2. 담겨진 결과를 판별했을 때 ?  참이면 보여지거나 실핼할 가능 설정
+    *                           :  거짓이면 보여지거나 실행할 기능 설정
+    *
+    *
+    * if(조건식){
+    *    조건식이 true 일 경우 실행
+    * } else{
+    *
+    * 조건식이 false 일 경우 실행
+    * }
+    *
+    * 에서 if 와(),{} 를 생력하여 =? : 으로 표현한 연산식
+    * */
+
+        String result = isTrue ? "짝수":"홀수";
+        System.out.printf("%d 은/는 %s 입니다.",input,result);
+    }
+
+
 }
