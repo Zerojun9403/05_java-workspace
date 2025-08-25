a9da26b41e7c060895a75ab3a6da86690e9d10e4
JOOYEONGJUN <galn7071@naver.com>
2025-08-25 18:06:31 +0900

    숙제시작

--- src/edu/practice/day1/Exercise1.java
+++ src/edu/practice/day1/Exercise1.java
@@ -1,4 +1,29 @@
 package edu.practice.day1;
 
 public class Exercise1 {
+
+    public  void Ex1(){
+
+
+
+        /*
+        *
+        * printf 를 이용해서
+        *=== 개인정보 ===
+            이름: 김자바
+            나이: 25세
+            키: 175.5cm
+            성별: M
+            취미: 프로그래밍
+        *
+        * */
+        String name = "김자바";
+        int age = 25;
+        double height = 175.5;
+        char gender = 'M';
+        String hobby = "프로그래밍";
+
+        System.out.printf("이름 : %d , 나이 : %d , 키 : %d, 성별 : %d, 취미 : %d");
+    }
+
 }
---
+++ src/edu/practice/day1/ExerciseRun.java
@@ -0,0 +1,8 @@
+package edu.practice.day1;
+
+import edu.op.ex.OperatorEx;
+
+public class ExerciseRun {
+
+    Exercise1 exRun = new Exercise1();
+}
