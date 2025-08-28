6c02d31828fbc61120b7d9332c1972091b9babb4
JOOYEONGJUN <galn7071@naver.com>
2025-08-28 10:33:34 +0900

    초기화/static 초기화

---
+++ src/edu/oop/field/pack3/StaticTestRun.java
@@ -0,0 +1,35 @@
+package edu.oop.field.pack3;
+
+public class StaticTestRun {
+    public static void main(String[] args) {
+
+        // koran 객체 생성
+        korean k1 = new korean(); // 클래스라는 문서에 작성된 내용들을 객체 로 설정
+                                  // 객체 이름 = k1
+        korean k2 = new korean();
+
+        System.out.println("k1의 nationalCode : " + k1.nationalCode);
+        System.out.println("k1의 nationalCode : " + k2.nationalCode);
+
+        //k1을 이용해서 nationalCode 변경
+        k1.nationalCode = 1;
+        System.out.println("------------ nationalCode 변경 후 ------------");
+        System.out.println("k1의 nationalCode : " + k1.nationalCode);
+        System.out.println("k1의 nationalCode : " + k2.nationalCode);
+
+        /*
+        * static 변수는 공용(공유) 변수!
+        * 객체가 100개 생성되어도
+        * 변수 하나로 모두가 공유
+        * -> 데이터베이스 접속하는 경로 변경
+        *   static {
+        *        nationalCode = 10;
+        *       }
+        *
+        * */
+
+        System.out.println("우리의 조상님 : " +korean.ANCESTOR); // 사용 가능
+        System.out.println();
+    }
+
+}
