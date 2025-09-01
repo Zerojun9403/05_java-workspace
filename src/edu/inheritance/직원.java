e419846f4d66cba29f92017234b247e643fe075e
JOOYEONGJUN <galn7071@naver.com>
2025-09-01 12:39:39 +0900

    상속 실습

--- src/edu/inheritance/직원.java
+++ src/edu/inheritance/직원.java
@@ -5,26 +5,68 @@ public class 직원 {
     private  String eName;   //사원이름
     private  int eId;        //사원번호
     private double baseSalary;//기본급
-    private String deparment;//부서
+    private String department;//부서
 
     //메서드 필드 이외에는 거의 다 기능 = 메서드 종류들
     // 생성자 매개변수 생성자
 
 
-    public 직원(String eName, int eId, double baseSalary, String deparment) {
+    public 직원(String eName, int eId, double baseSalary, String department) {
         this.eName = eName;
         this.eId = eId;
         this.baseSalary = baseSalary;
-        this.deparment = deparment;
+        this.department = department;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public int geteId() {
+        return eId;
+    }
+
+    public void seteId(int eId) {
+        this.eId = eId;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
 
     //공통기능
     public void info(){
         System.out.println("=== 직원정보 ===");
-        System.out.println("이름 : " + eName);
-        System.out.println("사번 : " + eId);
-        System.out.println("부서 : " + deparment);
-        System.out.println("기본급 : " + baseSalary + "원");
+        System.out.println("이름 : " + geteName());
+        System.out.println("사번 : " + geteId());
+        System.out.println("부서 : " + getDepartment());
+        System.out.println("기본급 : " + getBaseSalary() + "원");
 
     }
+    /* 추후 과제
+     // 급여 계산 - 자식 클래스에서 구체적으로 구현
+    public double calculateSalary(){
+        return  baseSalary;
+    }
+    */
+    // 업무 수행 - 자식 클래스에서 구체적으로 구현
+     public  void  work(){
+         System.out.println(geteName() + "님이 업무를 수행합니다.");
+     }
 }
---
+++ src/edu/inheritance/회사프로그램실행.java
@@ -0,0 +1,40 @@
+package edu.inheritance;
+
+public class 회사프로그램실행 {
+
+    public static void main(String[] args) {
+
+        System.out.println("==== 더조은 직원 관리시스템 =====");
+
+        //직원 총 3명 대표제외
+
+        개발자 dev1 = new 개발자("김개발",1001,4000000,"Java","맥북");
+        개발자 dev2 = new 개발자("이개발",1002,4000000,"Html","윈도우");
+
+        디자이너 des1 = new 디자이너("박디자인",3001,3000000,"design","Figma");
+
+        매니저 mgr1 = new 매니저("최매니저",2001,8000000,"개발1팀",8,50000);
+
+
+        //dev1 의 직원정보출력
+        dev1.info();
+        dev1.work();
+        dev1.codeReview();
+
+        //dev2 의 직원 정보 출력
+        dev2.info();
+        dev2.work();
+        dev2.codeReview();
+
+
+        // des1 직원정보출력
+        des1.info();
+        des1.work();
+
+
+        //mer1 직원정보 출력
+        mgr1.info();
+        mgr1.work();
+        mgr1.meeting();
+    }
+}
