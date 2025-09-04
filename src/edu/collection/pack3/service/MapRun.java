76feeecc165af861f868c71b4c8182eaa27ad203
JOOYEONGJUN <galn7071@naver.com>
2025-09-04 15:54:10 +0900

    컬렉션 ( Map - HashMap )

---
+++ src/edu/collection/pack3/service/MapRun.java
@@ -0,0 +1,10 @@
+package edu.collection.pack3.service;
+
+public class MapRun {
+
+
+    public static void main(String[] args) {
+        MapService mapService = new MapService();
+        mapService.method1();
+    }
+}
