6e9a1d616a77ecec12d75d9f2dfc61cfd18919e7
JOOYEONGJUN <galn7071@naver.com>
2025-09-05 11:20:11 +0900

    Exception

---
+++ src/edu/exception/pack1/ExceptionEx1.java
@@ -0,0 +1,12 @@
+package edu.exception.pack1;
+
+public class ExceptionEx1 {
+    /*
+
+    Exception : 코드로 처리 가능한 에러
+        ( 예 외 )
+
+    Exception Handling : Exception 발생시 처리하는 구문
+        ( 예 외 처 리 )
+    */
+}
---
+++ src/edu/exception/pack1/ExceptionRun1.java
@@ -0,0 +1,6 @@
+package edu.exception.pack1;
+
+
+// 단순한 실행만 할 곳
+public class ExceptionRun1 {
+}
