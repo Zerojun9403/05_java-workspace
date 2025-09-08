cd276fb8cacceefe35aaa517198a62bba9a3e946
JOOYEONGJUN <galn7071@naver.com>
2025-09-08 11:26:41 +0900

    IO(출력)

---
+++ src/edu/io/pack1/Run/FileRun.java
@@ -0,0 +1,15 @@
+package edu.io.pack1.Run;
+
+import edu.io.pack1.Service.FileService;
+
+public class FileRun {
+    public static void main(String[] args) {
+        FileService fs = new FileService();
+        //fs.method1();
+        //fs.method2();
+        //fs.method3();
+        fs.method4();
+
+    }
+
+}
